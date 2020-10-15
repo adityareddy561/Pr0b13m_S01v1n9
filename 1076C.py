f65f15667a26d44e2ac1337e0a1343c0f5cf7682
aditya reddy <adityareddy561@gmail.com>
2020-10-15 21:21:57 +0530

    Update 1076C.py

--- 1076C.py
+++ 1076C.py
@@ -5,4 +5,4 @@ for i in range(t):
     if(d**2>=4*d):
         print("Y",'{0:.9f}'.format(round((math.sqrt(d**2-4*d)+d)/2,9)),'{0:.9f}'.format(round((-math.sqrt(d**2-4*d)+d)/2,9)))
     else:
-        print("N")
+        print("N",end="")
